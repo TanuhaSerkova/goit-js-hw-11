b344c2cbf0d1d6643cfbb564155c89ab151e35bd
TanuhaSerkova <89786945+TanuhaSerkova@users.noreply.github.com>
2022-04-01 01:22:23 +0300

    js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css'
-import { fetchImages } from './js/fetch-images'
+import { fetchImages } from './js/fetch-images.js'
 import { renderGallery } from './js/render-gallery'
 import { onScroll, onToTopBtn } from './js/scroll'
 import Notiflix from 'notiflix'
@@ -21,66 +21,66 @@ onScroll()
 onToTopBtn()
 
 function onSearchForm(e) {
-  e.preventDefault()
-  window.scrollTo({ top: 0 })
-  page = 1
-  query = e.currentTarget.searchQuery.value.trim()
-  gallery.innerHTML = ''
-  loadMoreBtn.classList.add('is-hidden')
+    e.preventDefault()
+    window.scrollTo({ top: 0 })
+    page = 1
+    query = e.currentTarget.searchQuery.value.trim()
+    gallery.innerHTML = ''
+    loadMoreBtn.classList.add('is-hidden')
 
-  if (query === '') {
+    if (query === '') {
     alertNoEmptySearch()
     return
-  }
+    }
 
-  fetchImages(query, page, perPage)
+    fetchImages(query, page, perPage)
     .then(({ data }) => {
-      if (data.totalHits === 0) {
+    if (data.totalHits === 0) {
         alertNoImagesFound()
-      } else {
+    } else {
         renderGallery(data.hits)
         simpleLightBox = new SimpleLightbox('.gallery a').refresh()
         alertImagesFound(data)
 
         if (data.totalHits > perPage) {
-          loadMoreBtn.classList.remove('is-hidden')
+        loadMoreBtn.classList.remove('is-hidden')
         }
-      }
+    }
     })
     .catch(error => console.log(error))
 }
 
 function onLoadMoreBtn() {
-  page += 1
-  simpleLightBox.destroy()
+    page += 1
+    simpleLightBox.destroy()
 
-  fetchImages(query, page, perPage)
+    fetchImages(query, page, perPage)
     .then(({ data }) => {
-      renderGallery(data.hits)
-      simpleLightBox = new SimpleLightbox('.gallery a').refresh()
+    renderGallery(data.hits)
+    simpleLightBox = new SimpleLightbox('.gallery a').refresh()
 
-      const totalPages = Math.ceil(data.totalHits / perPage)
+    const totalPages = Math.ceil(data.totalHits / perPage)
 
-      if (page > totalPages) {
+    if (page > totalPages) {
         loadMoreBtn.classList.add('is-hidden')
         alertEndOfSearch()
-      }
+    }
     })
     .catch(error => console.log(error))
 }
 
 function alertImagesFound(data) {
-  Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
 }
 
 function alertNoEmptySearch() {
-  Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.')
+    Notiflix.Notify.failure('The search string cannot be empty. Please specify your search query.')
 }
 
 function alertNoImagesFound() {
-  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.')
 }
 
 function alertEndOfSearch() {
-  Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
+    Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")
 }
